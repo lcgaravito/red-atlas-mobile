c4e402faf236df9b75e5666e5a02af161af1ceb1
Luis Carlos Garavito Romero <lc.garavito@uniandes.edu.co>
2022-11-03 01:00:59 -0500

    add colors constant

---
+++ src/constants/COLORS.ts
@@ -0,0 +1,9 @@
+export const COLORS = {
+  primary: "#2CB9B0",
+  secondary: "#0C0D34",
+  white: "#FFFFFF",
+  black: "#000000",
+  grey: "#8A8D90",
+  lightGrey: "#F4F0EF",
+  darkGrey: "#8A8D90",
+};
