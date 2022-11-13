67c3aa0236f4689e8f2164494c5c026ed134a5c3
luis-redatlas <luis.garavito@atlas.red>
2022-11-13 18:23:27 -0500

    colors and fonts added

--- src/constants/COLORS.ts
+++ src/constants/COLORS.ts
@@ -1,9 +1,12 @@
 export const COLORS = {
-  primary: "#2CB9B0",
-  secondary: "#0C0D34",
+  primary: "#d31216",
+  secondary: "#333F48",
   white: "#FFFFFF",
   black: "#000000",
   grey: "#8A8D90",
   lightGrey: "#F4F0EF",
   darkGrey: "#8A8D90",
+  success: "#0e8e48",
+  warning: "#ff9f43",
+  danger: "#d31216",
 };
