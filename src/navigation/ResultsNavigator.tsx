faee035e905bf86024fde4a156d3e28567a5f565
lcgaravito <lc.garavito@uniandes.edu.co>
2022-11-14 01:25:56 -0500

    deploy and red atlas banner

--- src/navigation/ResultsNavigator.tsx
+++ src/navigation/ResultsNavigator.tsx
@@ -1,13 +1,36 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Image, TouchableOpacity } from "react-native";
+import { COLORS } from "../constants";
 import { ListingDetailScreen, ListingsScreen } from "../screens";
 import { ResultsStackParamList } from "./ResultsStackParamList";
+import Logotype from "../../assets/img/logotype.png";
 
 const Stack = createNativeStackNavigator<ResultsStackParamList>();
 
 const ResultsNavigator = () => {
   return (
-    <Stack.Navigator initialRouteName="Results">
-      <Stack.Screen name="Results" component={ListingsScreen} />
+    <Stack.Navigator
+      initialRouteName="Results"
+      screenOptions={{
+        headerTintColor: COLORS.primary,
+        headerTitleStyle: {
+          fontFamily: "Avenir",
+          fontWeight: "bold",
+          fontSize: 20,
+        },
+      }}
+    >
+      <Stack.Screen
+        name="Results"
+        component={ListingsScreen}
+        options={({ navigation }) => ({
+          headerTitle: () => (
+            <TouchableOpacity onPress={() => navigation.navigate("Results")}>
+              <Image style={{ width: 211, height: 47 }} source={Logotype} />
+            </TouchableOpacity>
+          ),
+        })}
+      />
       <Stack.Screen name="ListingDetail" component={ListingDetailScreen} />
     </Stack.Navigator>
   );
